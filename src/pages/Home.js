f9e00b29b082aede2c98894000f8e38ac21271db
Richmond Oduro <richmond.eo@gmail.com>
2020-02-19 13:30:06 +0100

    react beach resort application

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom'
+import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRoom';
+
+const Home = () => {
+    return (
+        <>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe room starting at $1299"> 
+                    <Link to="/rooms" className="btn-primary">Our rooms</Link>
+                </Banner>     
+            </Hero>
+            <Services />
+            <FeaturedRooms />
+        </>
+    )
+}
+
+export default Home;
